4903e02f3016f2b856e8c57e465997829eb265dd
Prakash.A.S <prakash.a.s@merckgroup.com>
2024-07-01 12:48:19 +0530

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,8 @@
+/* function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+ */
+const reverseString = (str) => str.split("").reverse().join("");
+module.exports = reverseString;
+
+console.log(reverseString("Hello World"));
